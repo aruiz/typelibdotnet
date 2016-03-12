d3ca1f8fd1f390a638e712e72a8a93c597b1b406
Alberto Ruiz <aruiz@gnome.org>
2016-03-12 03:17:00 +0000

    Add FunctionBlob, ArgBlob, SignatureBlob and SimpleTypeBlob structs

--- Program.cs
+++ Program.cs
@@ -20,6 +20,38 @@ namespace GLib.Typelib
 		UNION
 	}
 
+	internal enum TypeTag : ushort {
+		VOID,
+		BOOLEAN,
+		INT8,
+		UINT8,
+		INT16,
+		UINT16,
+		INT32,
+		UINT32,
+		INT64,
+		UINT64,
+		FLOAT,
+		DOUBLE,
+		GTYPE,
+		UTF8,
+		FILENAME,
+		ARRAY,
+		INTERFACE,
+		GLIST,
+		GSLIST,
+		GHASH,
+		ERROR,
+		UNICHAR
+	}
+
+	internal enum ScopeType : ushort {
+		INVALID,
+		CALL,
+		ASYNC,
+		NOTIFIED
+	}
+
 	[StructLayout(LayoutKind.Sequential, Pack = 1)]
 	struct Header {
 		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
@@ -76,8 +108,158 @@ namespace GLib.Typelib
 		public uint name;
 		public uint offset;
 
-		public bool GetIsLocal  () {
+		public bool IsLocal  () {
 			return (raw_local_reserved & 0x01) == 1 ? true : false ;
+		}
+	}
+
+	struct SimpleTypeBlobFlags {
+		/* guint reserved   : 8;
+		guint reserved2  :16;
+		guint pointer    : 1;
+		guint reserved3  : 2;
+		guint tag        : 5; */
+		uint reserved;
+
+		public bool IsPointer () {
+			return false;
+		}
+		public TypeTag GetTag () {
+			return TypeTag.VOID;
+		}
+	}
+
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	struct SimpleTypeBlob {
+		SimpleTypeBlobFlags flags;
+		uint offset;
+	}
+
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	struct ArgBlob {
+		uint        name;
+
+		/*guint          in                           : 1;
+		guint          out                          : 1;
+		guint          caller_allocates             : 1;
+		guint          nullable                     : 1;
+		guint          optional                     : 1;
+		guint          transfer_ownership           : 1;
+		guint          transfer_container_ownership : 1;
+		guint          return_value                 : 1;
+		guint          scope                        : 3;
+		guint          skip                         : 1; */
+		uint          reserved; /* 20 */
+		byte          closure;
+		byte          destroy;
+
+		ushort        padding;
+
+		SimpleTypeBlob arg_type;
+
+		public bool IsIn () {
+			return false;
+		}
+		public bool IsOut () {
+			return false;
+		}
+		public bool CallerAllocates () {
+			return false;
+		}
+		public bool Nullable () {
+			return false;
+		}
+		public bool Optional () {
+			return false;
+		}
+		public bool TransferOwnership () {
+			return false;
+		}
+		public bool TransferContainerOwnership () {
+			return false;
+		}
+		public bool IsReturnValue () {
+			return false;
+		}
+		public ScopeType GetScope () {
+			return ScopeType.INVALID;
+		}
+		public bool Skip () {
+			return false;
+		}
+	}
+
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	struct Signatureblob {
+		SimpleTypeBlob return_type;
+
+		/*guint16        may_return_null              : 1;
+		guint16        caller_owns_return_value     : 1;
+		guint16        caller_owns_return_container : 1;
+		guint16        skip_return                  : 1;
+		guint16        instance_transfer_ownership  : 1;
+		guint16        throws                       : 1; */
+		ushort        reserved; /* :10 */
+		ushort        n_arguments;
+		/* ArgBlob[n_arguments] */
+
+		public bool MayReturnNull () {
+			return false;
+		}
+		public bool CallerOwnsReturnValue () {
+			return false;
+		}
+		public bool CallerOwnsReturnContainer () {
+			return false;
+		}
+		public bool SkipReturn () {
+			return false;
+		}
+		public bool InstanceTransferOwnership () {
+			return false;
+		}
+		public bool Throws () {
+			return false;
+		}
+		public ArgBlob[] GetArguments (BinaryReader reader, long offset) {
+			return new ArgBlob[0];
+		}
+	}
+
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	struct FunctionBlob {
+		ushort type;
+		internal ushort flags;
+		uint name;
+		uint symbol;
+		uint signature;
+		ushort reserved;
+		ushort reserved2;
+
+
+		public bool IsDeprecated () {
+			return false;
+		}
+		public bool IsSetter () {
+			return false;
+		}
+		public bool IsGetter () {
+			return false;
+		}
+		public bool IsConstructor () {
+			return false;
+		}
+		public bool WrapsVFunc () {
+			return false;
+		}
+		public bool Throws () {
+			return false;
+		}
+		public bool GetIndex () {
+			return false;
+		}
+		public bool IsStatic () {
+			return false;
 		}
 	}
 		
@@ -88,7 +270,7 @@ namespace GLib.Typelib
 		private List<DirEntry> directory;
 		public Parser ()
 		{
-			using (var reader = new BinaryReader (File.Open ("./Gst-1.0.typelib", FileMode.Open))) {
+			using (var reader = new BinaryReader (File.Open ("./Soup-2.4.typelib", FileMode.Open))) {
 				var data = reader.ReadBytes (Marshal.SizeOf(typeof(Header)));
 				h = (Header) Marshal.PtrToStructure (GCHandle.Alloc (data,
 					                                                 GCHandleType.Pinned).AddrOfPinnedObject (),
@@ -115,7 +297,7 @@ namespace GLib.Typelib
 			foreach (var e in directory) {
 				var pos = reader.BaseStream.Position;
 
-				Console.WriteLine("{0} {1} {2}", e.type.ToString(), e.GetIsLocal(), GetStringFromOffset (reader, (long) e.name));
+				Console.WriteLine("{0} {1} {2}", e.type.ToString(), e.IsLocal(), GetStringFromOffset (reader, (long) e.name));
 
 				reader.BaseStream.Position = pos;
 			}
